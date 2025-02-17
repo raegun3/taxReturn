dc0936c5061c53b6e23d26ae9ea69c3501c13b21
raegun2 <148833221+raegun2@users.noreply.github.com>
2025-02-17 10:13:13 +1000

    toggle button error-downgraded to bootstrap 5.2.3

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,14 +8,14 @@ function Navbar() {
     
 
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-1 fixed-top ">
-            <div className="container bg-dark">
+            <div className="container-fluid bg-dark">
                 <Link to="/" className="navbar-brand">Milestone Accountant</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu" aria-controls="navmenu" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navmenu">
-                    <ul className="navbar-nav ms-auto">
+                    <ul className="navbar-nav ms-auto ">
                         <li className="nav-item">
                             <Link to="Showcase1" className="nav-link">Tax Return</Link>
                         </li>
