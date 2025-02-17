bb571e1c82f390faa1952c340c87250717352913
raegun2 <148833221+raegun2@users.noreply.github.com>
2025-02-17 14:47:14 +1000

    form-control pt updated

--- src/pages/Accounting&Advisory.jsx
+++ src/pages/Accounting&Advisory.jsx
@@ -2,7 +2,9 @@
 
 export default function AccountingAdvisory() {
     return (
-            <div className="container align-text-center"><h1>page to be updated</h1></div>
+            <div className="container text-light">
+                div
+                <h1>page to be updated</h1></div>
         );      
 
 }
